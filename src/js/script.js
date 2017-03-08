23c19b298cdf72d39d2213076a0afa61ea7c03cb
Michal <m14lickiewicz@gmail.com>
2017-03-08 21:30:50 +0100

    Send value from form.

--- src/js/script.js
+++ src/js/script.js
@@ -2,19 +2,20 @@
  * Created by Michał_2 on 2017-03-07.
  */
 
- var CommentApp = {
-  //variables
+var CommentApp = {
+    //variables
     URL: 'https://jsonplaceholder.typicode.com',
+    COMMENT_ID: '0',
 
 
-  //init
-        init: function (){
-            CommentApp.getComments();
-        },
+    //init
+    init: function () {
+        CommentApp.getComments();
+        CommentApp.onSend($('.btn-add'));
+    },
 
 
-
-  //function
+    //function
     getComments: function () {
         $.ajax({
             url: CommentApp.URL + '/comments',
@@ -30,24 +31,24 @@
     },
 
     drawComments: function (comments) {
+        CommentApp.COMMENT_ID++;
         var box = "";
         box += '<div class="mui-col-xs-10 mui-col-xs-offset-1 padding0">';
         box += '<div class="mui-row comment-box">';
-         for (var i = 0; i < 15; i++){
-             box += '<div class="comment-detail mui-col-md-4 mui-col-lg-3 mui-col-xs-5 mui-col-xs-offset-1">';
-             box += '<a href="#"><i class="fa fa-trash" aria-hidden="true bin-delete" onclick="CommentApp.removeComments('+ comments[i].id +', event.target)"></a></i>';
-             box += '<span class="name">'+ comments[i].name +'</span>';
-             box += '<span class="body"><q>'+ comments[i].body +'</q></span>';
-             box += '<span class="email">'+ comments[i].email +'</span>';
-             box += '</div>';
-
+        for (var i = 0; i < 15; i++) {
+            box += '<div class="comment-detail mui-col-md-4 mui-col-lg-3 mui-col-xs-5 mui-col-xs-offset-1">';
+            box += '<a href="#"><i class="fa fa-trash" aria-hidden="true bin-delete" onclick="CommentApp.removeComments(' + comments[i].id + ', event.target)"></a></i>';
+            box += '<span class="name">' + comments[i].name + '</span>';
+            box += '<span class="body"><q>' + comments[i].body + '</q></span>';
+            box += '<span class="email">' + comments[i].email + '</span>';
+            box += '</div>';
         }
         box += '</div>';
         box += '</div>';
-
+        console.log(comments[i].id);
+        console.log(CommentApp.COMMENT_ID);
         $('.comments-section').append(box);
     },
-
 
     removeComments: function (id, e) {
         event.preventDefault();
@@ -55,11 +56,32 @@
         console.log(id);
         console.log($(e).parent().parent());
         $(e).closest('.comment-detail').remove();
+    },
+
+    onSend: function (btn) {
+        $(btn).on('click', function (e) {
+            CommentApp.getInputValue(btn);
+        });
+    },
+
+    getInputValue: function (btn) {
+        CommentApp.COMMENT_ID++;
+        console.log($(btn).parent().find('input[name=name]').val());
+        var titleValue = ($(btn).parent().find('input[name=name]').val());
+        var commentValue = ($(btn).parent().find('input[name=comment]').val());
+        var emailValue = ($(btn).parent().find('input[name=email]').val());
+
+        console.log(titleValue, commentValue, emailValue);
+        var block = "";
+        block += '<div class="comment-detail mui-col-md-4 mui-col-lg-3 mui-col-xs-5 mui-col-xs-offset-1">';
+        block += '<a href="#"><i class="fa fa-trash" aria-hidden="true bin-delete" onclick="CommentApp.removeComments(' + CommentApp.COMMENT_ID + ', event.target)"></a></i>';
+        block += '<span class="name">' + titleValue + '</span>';
+        block += '<span class="body"><q>' + commentValue + '</q></span>';
+        block += '<span class="email">' + emailValue + '</span>';
+        block += '</div>';
+
+        $('.comment-box').prepend(block);
     }
-
-
-
-
 
 
 };

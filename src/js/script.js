2768480c415b32d58f3ab65d4aedd2c9a724e8c0
Michal <m14lickiewicz@gmail.com>
2017-03-08 14:09:56 +0100

    Init new function and styling.

--- src/js/script.js
+++ src/js/script.js
@@ -21,13 +21,30 @@
             method: 'GET',
             success: function (response) {
                 console.log(response);
+                CommentApp.drawComments(response);
             },
             error: function () {
                 console.log('Getting data error!')
             }
         });
-    }
+    },
 
+    drawComments: function (data) {
+        var box = "";
+        box += '<div class="mui-col-xs-10 mui-col-xs-offset-1 padding0">';
+        box += '<div class="mui-row comment-box">';
+         for (var c = 0; c < 15; c++){
+             box += '<div class="comment-detail mui-col-md-4 mui-col-lg-3 mui-col-xs-5 mui-col-xs-offset-1">';
+             box += '<span class="name">'+ data[c].name +'</span>';
+             box += '<span class="body">'+ data[c].body +'</span>';
+             box += '<span class="email">'+ data[c].email +'</span>';
+             box += '</div>';
+        }
+        box += '</div>';
+        box += '</div>';
+
+        $('.comments-section').append(box);
+    }
 
 
 

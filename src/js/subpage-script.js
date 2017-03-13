2d6a070feed8ad2e27ce6b7c6b6fb8bd05c41dce
Michal <m14lickiewicz@gmail.com>
2017-03-13 14:13:38 +0100

    Set mustache in subpage.

--- src/js/subpage-script.js
+++ src/js/subpage-script.js
@@ -14,36 +14,45 @@ var userApp = {
             method: 'GET',
             success: function (response) {
                 console.log(response);
-                userApp.showView(response);
+                userApp.drawUsers(response);
             },
             error: function () {
                 console.log('Getting data error!')
             }
         });
     },
-
-    showView: function (users) {
-        userApp.drawUsers(users);
-    },
+    //drawUsers: function (users) {
+    //    var userBlock = '';
+    //    userBlock += '<div class="mui-row <!--flex-wrap-->">';
+    //    for (var i = 0; i < users.length; i++) {
+    //        userBlock += '<div class="user-box data-box padding0 mui-col-xs-10 mui-col-xs-offset-1">';
+    //        userBlock += '<div class="set-span  mui-col-md-10 mui-col-xs-8 padding0">';
+    //        userBlock += '<span>' + users[i].name + '</span>';
+    //        userBlock += '<span>' + users[i].email + '</span>';
+    //        userBlock += '<span>' + users[i].phone + '</span>';
+    //        userBlock += '<span>' + users[i].website + '</span>';
+    //        userBlock += '</div>';
+    //        userBlock += '<div class="mui-col-md-2 mui-col-xs-4 padding0 mui--text-center">';
+    //        userBlock += '<a href="#" class="go-to hide-users" onclick="userApp.removeUsers(' + users[i].id + ', event.target)">Hide</a>';
+    //        userBlock += '</div>';
+    //        userBlock += '</div>';
+    //    }
+    //    userBlock += '</div>';
+    //    $('.user-subpage-content').append(userBlock);
+    //},
 
     drawUsers: function (users) {
-        var userBlock = '';
-        userBlock += '<div class="mui-row <!--flex-wrap-->">';
-        for (var i = 0; i < users.length; i++) {
-            userBlock += '<div class="user-box data-box padding0 mui-col-xs-10 mui-col-xs-offset-1">';
-            userBlock += '<div class="set-span  mui-col-md-10 mui-col-xs-8 padding0">';
-            userBlock += '<span>' + users[i].name + '</span>';
-            userBlock += '<span>' + users[i].email + '</span>';
-            userBlock += '<span>' + users[i].phone + '</span>';
-            userBlock += '<span>' + users[i].website + '</span>';
-            userBlock += '</div>';
-            userBlock += '<div class="mui-col-md-2 mui-col-xs-4 padding0 mui--text-center">';
-            userBlock += '<a href="#" class="go-to hide-users" onclick="userApp.removeUsers('+ users[i].id +', event.target)">Hide</a>';
-            userBlock += '</div>';
-            userBlock += '</div>';
-        }
-        userBlock += '</div>';
-        $('.user-subpage-content').append(userBlock);
+        $.each(users, function (index, item) {
+            userApp.mustacheRender(item);
+        });
+    },
+
+    mustacheRender: function (item) {
+        var template = $('#template').html();
+        Mustache.parse(template);
+        var rendered = Mustache.render(template, item);
+        console.log(item);
+        $('#target').append(rendered);
     },
 
     removeUsers: function (id, btn) {
